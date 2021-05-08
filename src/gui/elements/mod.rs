d01c3f4133a9595a54c2c3c1a587b220deb9482b
MoAlyousef <may642_2000@hotmail.com>
2021-05-08 20:51:36 +0300

    update to fltk 1.0

--- src/gui/elements/mod.rs
+++ src/gui/elements/mod.rs
@@ -1,9 +1,8 @@
-
 use fltk::{
     button::*,
+    enums::{Color, Font, FrameType},
     frame::Frame,
-    frame::FrameType,
-    image::*,
+    prelude::*,
 };
 
 // getting asset folder contents
@@ -12,17 +11,13 @@ use fltk::{
 struct Assets;
 
 pub fn gen_login_button() -> Button {
-    let mut but = Button::default()
-    .with_size(70, 30)
-    .with_label("Login");
+    let mut but = Button::default().with_size(70, 30).with_label("Login");
     but.set_color(Color::from_u32(0x003F87));
     but.set_label_color(Color::from_u32(0xffffff));
     but.set_frame(FrameType::RFlatBox);
     but.set_selection_color(Color::from_u32(0x3498db));
     but
 }
-
-
 
 // pub fn gen_imgbanner(x:i32, y:i32, width: i32, height: i32) -> Frame {
 //     let mut banner_frame = Frame::new(x, y, width, height, "");
@@ -38,15 +33,15 @@ pub fn gen_login_button() -> Button {
 //     banner_frame
 // }
 
-pub fn gen_output_label_frame(x:i32, y:i32, width: i32, height: i32) -> Frame {
+pub fn gen_output_label_frame(x: i32, y: i32, width: i32, height: i32) -> Frame {
     let mut output_label_frame = Frame::new(x, y, width, height, "Ecclesiastica Login");
     {
         output_label_frame.set_frame(FrameType::FlatBox);
         output_label_frame.set_color(Color::from_rgb(0, 120, 254));
         output_label_frame.set_label_color(Color::from_u32(0xffffff));
         output_label_frame.set_label_size(24);
-        output_label_frame.set_label_font(fltk::Font::Helvetica);
-    }   
-    
+        output_label_frame.set_label_font(Font::Helvetica);
+    }
+
     output_label_frame
 }
--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,9 +1,13 @@
 use fltk::{
-    app, button::*, frame::*, window::*, image::*,
-    enums::Color, enums::LabelType, enums::Align,
-    prelude::*, menu::{MenuBar, MenuItem}, text::TextBuffer,
-};  //
-// use std::{thread, time};
+    app,
+    button::*,
+    enums::{Align, Color, FrameType},
+    frame::*,
+    image::*,
+    prelude::*,
+    window::*,
+}; //
+   // use std::{thread, time};
 
 mod elements; // accessing elements folder
 mod logic;
@@ -20,8 +24,9 @@ pub fn start_app() {
     let window_height: i32 = 600;
     let window_title: &str = "Ecclesiastica v.01";
     let app = app::App::default(); //.with_scheme(AppScheme::Gtk)
-    // app::set_visible_focus(false);
-    let mut win = Window::new(window_x, window_y, window_width, window_height, &window_title,);
+                                   // app::set_visible_focus(false);
+    let mut win = Window::new(window_x, window_y, window_width, window_height, None)
+        .with_label(&window_title);
 
     let img = Assets::get("app_icon.png").unwrap(); // these 2 lines to embed if not use line after next line only
     let image = PngImage::from_data(&img).unwrap();
@@ -29,46 +34,44 @@ pub fn start_app() {
 
     win.set_icon(Some(image));
 
-
-
-
-
     //let mut frame = Frame::new(0, 0, window_x, window_y, "HIIIII");
     let mut frame = Frame::default().size_of(&win);
     frame.set_frame(FrameType::FlatBox);
     // frame.set_color(Color::Blue); // now we are setting background in the frame
 
-
     // let mut banner_frame = elements::gen_imgbanner(0, 0, frame.width(), 100);
-    let mut output_label_frame = elements::gen_output_label_frame(0, 0, frame.width(), 50);
+    let output_label_frame = elements::gen_output_label_frame(0, 0, frame.width(), 50);
     // let mut but = elements::gen_button(frame);
     let mut close_button = Button::default()
-    .with_size(20,20)
-    .right_of(&output_label_frame, -20)
-    .with_align(fltk::Align::Inside | fltk::Align::Center)
-    // .with_align(fltk::Align::Right)
-    .with_label("X");
+        .with_size(20, 20)
+        .right_of(&output_label_frame, -20)
+        .with_align(Align::Inside | Align::Center)
+        // .with_align(Align::Right)
+        .with_label("X");
     close_button.set_color(Color::from_rgb(200, 200, 200));
     close_button.set_frame(FrameType::RFlatBox);
     close_button.set_selection_color(Color::from_rgb(250, 250, 250));
     close_button.hide();
 
     let mut pack = fltk::group::Pack::default()
-    .center_of(&frame)
-    .with_pos(frame.x() + frame.width()/4,  frame.y()+output_label_frame.height()+frame.height()/6)
-    .with_size(frame.width() / 2, 400);
+        .center_of(&frame)
+        .with_pos(
+            frame.x() + frame.width() / 4,
+            frame.y() + output_label_frame.height() + frame.height() / 6,
+        )
+        .with_size(frame.width() / 2, 400);
 
     let mut username_label = Frame::new(0, 0, pack.width(), 30, "Username");
     username_label.set_frame(FrameType::FlatBox);
-;
-    let mut username = fltk::input::Input::new(0, 0, pack.width() ,30, "");
+
+    let mut username = fltk::input::Input::new(0, 0, pack.width(), 30, "");
     username.set_value(&format!("{}", ""));
     username.set_color(Color::from_u32(0xffffff));
     username.set_tooltip("Type your username here");
 
     let mut password_label = Frame::new(0, 0, pack.width(), 30, "Password");
     password_label.set_frame(FrameType::FlatBox);
-    let mut password = fltk::input::SecretInput::new(0, 0, pack.width() ,30, "");
+    let mut password = fltk::input::SecretInput::new(0, 0, pack.width(), 30, "");
     password.set_value(&format!("{}", ""));
     password.set_color(Color::from_u32(0xffffff));
     password.set_tooltip("Type your password here");
@@ -81,52 +84,44 @@ pub fn start_app() {
     pack.end();
 
     let mut clone_close_button = close_button.clone();
-    let mut clone_win = win.clone();
-    login_button.set_callback( 
-        move ||
-        if username.value() == "santokalayil".to_string() {
-            if password.value() == "hi".to_string() {
+    let mut clone_pack = pack.clone();
+    login_button.set_callback(move |_| {
+        use logic::*;
+        match login_valid(&username.value(), &password.value()) {
+            LoginResult::CorrectCredentials => {
+                clone_pack.hide();
                 clone_close_button.show();
-                pack.hide();
-                clone_win.clear();
-            }
-            else {
+            },
+            LoginResult::InvalidPassword => {
                 space_after_loginbtn.set_label_size(16);
                 space_after_loginbtn.set_label_color(Color::from_u32(0xff0000));
                 space_after_loginbtn.set_label("password incorrect");
-            }
+            },
+            LoginResult::InvalidUserName => {
+                space_after_loginbtn.set_label_size(16);
+                space_after_loginbtn.set_label_color(Color::from_u32(0xff0000));
+                space_after_loginbtn.set_label("Invalid! Try again");
+            },
+
         }
-        else {
-            space_after_loginbtn.set_label_size(16);
-            space_after_loginbtn.set_label_color(Color::from_u32(0xff0000));
-            space_after_loginbtn.set_label("Invalid! Try again");
-        }
-    );
+    });
 
     let mut clone_close_button = close_button.clone();
-    close_button.set_callback( move || 
-        {
-            output_label_frame.hide();
-            clone_close_button.hide();
-        }
-    );
-    
-    // login_button.set_callback(move || logic::login("santokalayil", "hi"));
+    close_button.set_callback(move |_| {
+        clone_close_button.hide();
+        pack.show();
+    });
 
-
-
-
+    // login_button.set_callback(move |_| logic::login("santokalayil", "hi"));
 
     win.make_resizable(true);
     win.end();
-    win.show_with_args(&["-scheme","+gtk","-nokbd"]);
+    win.show_with_args(&["-scheme", "+gtk", "-nokbd"]);
     // win.show();
 
     // theming
-    app::background(229,229,255); // this is the app module imported and not he app
-    
+    app::background(229, 229, 255); // this is the app module imported and not he app
 
-    // but.set_callback(move || output_label_frame.set_label("Ecclesiastica v.0.1"));
+    // but.set_callback(move |_| output_label_frame.set_label("Ecclesiastica v.0.1"));
     app.run().unwrap();
 }
-
