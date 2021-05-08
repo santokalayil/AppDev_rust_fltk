8fa02235f57da008e69b91484b0e0ffd609dbdd2
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-05-08 09:46:10 +0530

    committing files

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+// #![windows_subsystem = "windows"]  // to hide console window
+
+
+// mod cmd_utilities;
+mod gui;
+// mod python;
+
+// use these if need to embed image
+#[macro_use]
+extern crate rust_embed;
+
+
+fn main() {
+    // cmd_utilities::run_command("python".to_string()); // argument needs to be added. find some way like vec or list
+    // python::show();
+    // python::print_from_python_module();
+    gui::start_app()
+}
