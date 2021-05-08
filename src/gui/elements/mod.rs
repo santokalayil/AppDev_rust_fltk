a1ee16d1870d019eee5210297be648dcc067af77
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-05-08 16:49:06 +0530

    committing files

--- src/gui/elements/mod.rs
+++ src/gui/elements/mod.rs
@@ -11,57 +11,41 @@ use fltk::{
 #[folder = "assets/"]
 struct Assets;
 
-// button
-
-// pub fn gen_button(frame:Frame) -> Button {
-//     let mut but = Button::default()
-//     .with_size(100, 40)
-//     // .size_of(&frame)
-//     .center_of(&frame)
-    
-//     // .Align(Align::Center)
-//     .with_label("AppVersion");
-//     //.with_pos(10, 10)
-//     //.with_size(80, 40)
-//     but.set_color(Color::from_rgb(88, 214, 141));
-//     but.set_frame(FrameType::RoundUpBox);
-    
-    
-//     // return button
-//     but
-// }
-
 pub fn gen_login_button() -> Button {
     let mut but = Button::default()
-    .with_size(100, 40)
+    .with_size(70, 30)
     .with_label("Login");
-    but.set_color(Color::from_rgb(88, 214, 141));
-    but.set_frame(FrameType::RoundUpBox);
+    but.set_color(Color::from_u32(0x003F87));
+    but.set_label_color(Color::from_u32(0xffffff));
+    but.set_frame(FrameType::RFlatBox);
+    but.set_selection_color(Color::from_u32(0x3498db));
     but
 }
 
 
 
-pub fn gen_imgbanner(x:i32, y:i32, width: i32, height: i32) -> Frame {
-    let mut banner_frame = Frame::new(x, y, width, height, "");
-    {
-        banner_frame.set_frame(FrameType::FlatBox);
-        banner_frame.set_color(Color::from_rgb(0, 120, 254));
-        // let banner_img = Assets::get("banner.svg").unwrap();
-        // let mut banner_img = SvgImage::from_data(&banner_img).unwrap();
-        let mut banner_img = SvgImage::load(&std::path::Path::new("assets\\banner.svg")).unwrap();
-        banner_img.scale(width, height, false, true);
-        banner_frame.set_image(Some(banner_img));
-    }
-    banner_frame
-}
+// pub fn gen_imgbanner(x:i32, y:i32, width: i32, height: i32) -> Frame {
+//     let mut banner_frame = Frame::new(x, y, width, height, "");
+//     {
+//         banner_frame.set_frame(FrameType::FlatBox);
+//         banner_frame.set_color(Color::from_rgb(0, 120, 254));
+//         // let banner_img = Assets::get("banner.svg").unwrap();
+//         // let mut banner_img = SvgImage::from_data(&banner_img).unwrap();
+//         let mut banner_img = SvgImage::load(&std::path::Path::new("assets\\banner.svg")).unwrap();
+//         banner_img.scale(width, height, false, true);
+//         banner_frame.set_image(Some(banner_img));
+//     }
+//     banner_frame
+// }
 
 pub fn gen_output_label_frame(x:i32, y:i32, width: i32, height: i32) -> Frame {
     let mut output_label_frame = Frame::new(x, y, width, height, "Ecclesiastica Login");
     {
         output_label_frame.set_frame(FrameType::FlatBox);
         output_label_frame.set_color(Color::from_rgb(0, 120, 254));
-        output_label_frame.set_label_color(Color::from_rgb(200, 200, 200));
+        output_label_frame.set_label_color(Color::from_u32(0xffffff));
+        output_label_frame.set_label_size(24);
+        output_label_frame.set_label_font(fltk::Font::Helvetica);
     }   
     
     output_label_frame
