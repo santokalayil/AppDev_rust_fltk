165acd6f50d1b8243fb3ca818eb3f31cae7d84bb
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-05-08 16:48:36 +0530

    committing files

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ extern crate winres;
 #[cfg(windows)]
 fn main() {
     let mut res = winres::WindowsResource::new();
-    res.set_icon("assets/binary_icon.ico");
+    res.set_icon("assets/app_icon.ico");
     res.compile().unwrap();
 }
 
