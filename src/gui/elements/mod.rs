377cd38a95826d160a9d07570eab87b285eaa3b8
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-05-09 13:39:56 +0530

    committing files

--- src/gui/elements/mod.rs
+++ src/gui/elements/mod.rs
@@ -19,20 +19,6 @@ pub fn gen_login_button() -> Button {
     but
 }
 
-// pub fn gen_imgbanner(x:i32, y:i32, width: i32, height: i32) -> Frame {
-//     let mut banner_frame = Frame::new(x, y, width, height, "");
-//     {
-//         banner_frame.set_frame(FrameType::FlatBox);
-//         banner_frame.set_color(Color::from_rgb(0, 120, 254));
-//         // let banner_img = Assets::get("banner.svg").unwrap();
-//         // let mut banner_img = SvgImage::from_data(&banner_img).unwrap();
-//         let mut banner_img = SvgImage::load(&std::path::Path::new("assets\\banner.svg")).unwrap();
-//         banner_img.scale(width, height, false, true);
-//         banner_frame.set_image(Some(banner_img));
-//     }
-//     banner_frame
-// }
-
 pub fn gen_output_label_frame(x: i32, y: i32, width: i32, height: i32) -> Frame {
     let mut output_label_frame = Frame::new(x, y, width, height, "Ecclesiastica Login");
     {
