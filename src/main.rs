feb438d196353318553e6580ee756331eb733d88
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-05-10 20:59:11 +0530

    committing files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![windows_subsystem = "windows"] // comment this line to show console window
 
 // mod cmd_utilities;
-mod gui;
+mod views;
 // mod python;
 
 // use these if need to embed image
@@ -12,5 +12,5 @@ fn main() {
     // cmd_utilities::run_command("python".to_string()); // argument needs to be added. find some way like vec or list
     // python::show();
     // python::print_from_python_module();
-    gui::start_app()
+    views::start_app()
 }
